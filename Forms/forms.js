f2d8507c3070bce508d964c3e204b2b4938627a2
Viktorh <ezuvictor@gmail.com>
2025-02-08 21:09:37 -0800

    i added another repo and some updates on forms

--- Forms/forms.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector("button");
-btn.addEventListener("click", () => {
-  btn.textContent = "YOU CLICKED ME!! ❤️";
-  setTimeout(() => {
-    btn.textContent = "Press me";
-  }, 1000);
-});
